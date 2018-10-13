4ac4609656ff7b197438e24b55bb0aa6d705ebae
lmatsuki <lmatsuki87@gmail.com>
2018-10-13 13:17:51 -0700

    Added trigger colliders to player and enemy ships.

---
+++ Assets/Scripts/ShipHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipHealth : MonoBehaviour {
+
+    public int health;
+
+    private bool alive;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void takeDamage()
+    {
+        if (health > 0 && alive)
+        {
+            health--;
+
+            if (health == 0)
+            {
+                die();
+            }
+        }
+    }
+
+    void die()
+    {
+        alive = false;
+        gameObject.SetActive(false);
+    }
+}
