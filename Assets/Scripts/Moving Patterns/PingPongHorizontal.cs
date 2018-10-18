fc7c82e97fd52fb63b4ffa417fb2def3e3b190b8
lmatsuki <lmatsuki87@gmail.com>
2018-10-17 20:53:05 -0700

    Added moving pattern.

---
+++ Assets/Scripts/Moving Patterns/PingPongHorizontal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongHorizontal : MonoBehaviour
+{
+    public bool moving;
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+		
+	}
+}
