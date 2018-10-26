f01f0ccd3e728dd48018991c0101bdecced4a55f
lmatsuki <lmatsuki87@gmail.com>
2018-10-25 23:19:43 -0700

    Added boundaries that destroy gameObjects on enter.

---
+++ Assets/Scripts/MapBoundary.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapBoundary : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerBullet") || other.CompareTag("EnemyBullet"))
+        {
+            // Replace with Object pooler's remove method
+            Destroy(other.transform.parent.gameObject);
+        }
+    }
+}
