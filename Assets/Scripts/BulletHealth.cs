ada396854e66889e0825078238fb592c9680e915
lmatsuki <lmatsuki87@gmail.com>
2018-12-02 17:53:24 -0800

    Made WeakBullets destructible

---
+++ Assets/Scripts/BulletHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHealth : MonoBehaviour
+{
+    public int health;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if (health <= 0)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerBullet"))
+        {
+            print(other.name);
+            health--;
+        }
+    }
+}
