d5a82e9d127ed326cceb0733f17635121ac1ed1e
lmatsuki <lmatsuki87@gmail.com>
2019-01-06 17:07:53 -0800

    Hooked up volume slider

---
+++ Assets/Scripts/Menus/SettingsMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+}
