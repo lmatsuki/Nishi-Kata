53d40c18ead92ece8de046b53382c9cde747f7b1
lmatsuki <lmatsuki87@gmail.com>
2019-01-06 19:12:16 -0800

    Added cardinal direction firing pattern

---
+++ Assets/Scripts/Fire Patterns/BulletTargeter/CardinalDirectionTargeter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CardinalDirectionTargeter : BaseBulletTargeter
+{
+    private int currentBulletIndex;
+    private float baseRotation;
+
+    public void Start()
+    {
+        currentBulletIndex = 0;
+    }
+
+    public override void TargetBullet(Transform bullet)
+    {
+        bullet.Rotate(0f, (90f * currentBulletIndex + baseRotation), 0f);
+
+        IncrementBulletIndex();
+    }
+
+    void IncrementBulletIndex()
+    {
+        currentBulletIndex++;
+        baseRotation += 10;
+
+        if (currentBulletIndex >= 4)
+        {
+            currentBulletIndex = 0;
+        }
+    }
+}
