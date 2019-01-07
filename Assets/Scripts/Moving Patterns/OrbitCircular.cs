0971291c9dfdb3b2e867816276701f21c55445bc
lmatsuki <lmatsuki87@gmail.com>
2019-01-06 19:41:01 -0800

    Added circular movement pattern

---
+++ Assets/Scripts/Moving Patterns/OrbitCircular.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class OrbitCircular : BaseMovement
+{
+    public float RotateSpeed;
+    public float Radius;
+
+    private Vector3 center;
+    private float angle;
+
+    void Start()
+    {
+        center = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        angle += RotateSpeed * Time.deltaTime;
+
+        Vector3 offset = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * Radius;
+        Vector3 offsetPosition = center + offset;
+        transform.position = new Vector3(offsetPosition.x, transform.position.y, offsetPosition.z);
+    }
+}
