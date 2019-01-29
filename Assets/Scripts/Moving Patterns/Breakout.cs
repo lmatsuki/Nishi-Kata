4fef6d71e41eab0d3024721a422e99eee2a5e8b0
lmatsuki <lmatsuki87@gmail.com>
2019-01-28 21:32:00 -0800

    added breakout movement pattern

---
+++ Assets/Scripts/Moving Patterns/Breakout.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Breakout : BaseMovement
+{
+    public float speed;
+
+    private new Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+        RotateRandom();
+    }
+
+    void FixedUpdate()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+
+        rigidbody.velocity = transform.forward * speed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(Tags.Wall))
+        {
+            ChangeDirection();
+        }
+    }
+
+    private void RotateRandom()
+    {
+        transform.Rotate(0f, Random.Range(0, 360), 0f);
+    }
+
+    private void ChangeDirection()
+    {
+        // get the angle the enemy is facing
+        // 0 -> 90 && 180 -> 270 = turn right
+        // 90 -> 180 && 270 -> 360 = turn left
+    }
+}
