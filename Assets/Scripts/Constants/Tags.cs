90bc86c8004074cab60792984e40ce196f1214e1
lmatsuki <lmatsuki87@gmail.com>
2019-01-28 21:32:14 -0800

    tagged walls with Wall

--- Assets/Scripts/Constants/Tags.cs
+++ Assets/Scripts/Constants/Tags.cs
@@ -3,4 +3,5 @@
     public const string PlayerBullet = "PlayerBullet";
     public const string EnemyBullet = "EnemyBullet";
     public const string Bullet = "Bullet";
+    public const string Wall = "Wall";
 }
