955689105a7f62516edd1d374079ca28867b483e
lmatsuki <lmatsuki87@gmail.com>
2019-01-28 20:48:58 -0800

    added QuintupleTargeter

---
+++ Assets/Scripts/Fire Patterns/BulletTargeter/PlayerQuintupleTargeter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerQuintupleTargeter : BaseBulletTargeter
+{
+    public Transform player;
+    public float angleBetweenBullets;
+
+    private int currentBulletIndex;
+
+    public void Start()
+    {
+        currentBulletIndex = -2;
+    }
+
+    public override void TargetBullet(Transform bullet)
+    {
+        bullet.LookAt(player);
+        bullet.Rotate(0f, (angleBetweenBullets * currentBulletIndex), 0f);
+
+        IncrementBulletIndex();
+    }
+
+    void IncrementBulletIndex()
+    {
+        currentBulletIndex++;
+
+        if (currentBulletIndex >= 3)
+        {
+            currentBulletIndex = -2;
+        }
+    }
+}
