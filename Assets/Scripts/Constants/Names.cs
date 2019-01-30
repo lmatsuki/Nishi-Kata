979f17c610596543a4571f415899230716909217
lmatsuki <lmatsuki87@gmail.com>
2019-01-29 22:13:21 -0800

    added names constants

---
+++ Assets/Scripts/Constants/Names.cs
@@ -0,0 +1,7 @@
+﻿public class Names
+{
+    public const string LeftWall = "Left Wall";
+    public const string RightWall = "Right Wall";
+    public const string TopWall = "Top Wall";
+    public const string BottomWall = "Bottom Wall";
+}
