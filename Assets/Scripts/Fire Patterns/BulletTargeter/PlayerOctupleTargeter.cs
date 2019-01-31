c787caff03bafc2dd85ac1fc170b2cb39a9b07fd
lmatsuki <lmatsuki87@gmail.com>
2019-01-30 20:52:36 -0800

    implemented 8-direction targetting

---
+++ Assets/Scripts/Fire Patterns/BulletTargeter/PlayerOctupleTargeter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerOctupleTargeter : BaseBulletTargeter
+{
+    public Transform player;
+    public float angleBetweenBullets;
+
+    private int currentBulletIndex;
+
+    public void Start()
+    {
+        currentBulletIndex = 0;
+    }
+
+    public override void TargetBullet(Transform bullet)
+    {
+        bullet.LookAt(player);
+        bullet.Rotate(0f, (angleBetweenBullets * currentBulletIndex), 0f);
+
+        IncrementBulletIndex();
+    }
+
+    void IncrementBulletIndex()
+    {
+        currentBulletIndex++;
+
+        if (currentBulletIndex >= 8)
+        {
+            currentBulletIndex = 0;
+        }
+    }
+}
