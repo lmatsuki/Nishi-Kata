3bff5c2cbc6e7ca63eb69a1dde9ca939dc7033b3
lmatsuki <lmatsuki87@gmail.com>
2019-02-02 00:28:51 -0800

    added menu theme and replaced boss theme

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -3,8 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Start()
+    {
+        AudioManager.instance.PlaySong(Songs.MenuTheme);
+    }
+
     public void PlayGame()
     {
+        AudioManager.instance.StopSong(Songs.MenuTheme);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
