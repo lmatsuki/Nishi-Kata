293889b9e2ee40582f59aa6d39addb4086123dc3
lmatsuki <lmatsuki87@gmail.com>
2019-02-28 22:39:35 -0800

    added script to trigger effect when moving

---
+++ Assets/Scripts/MovingEffect.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class MovingEffect : MonoBehaviour
+{
+    public GameObject movingParticleFX;
+
+    private GameObject currentMovingFX;
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        currentMovingFX = Instantiate(movingParticleFX, transform);
+        currentMovingFX.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (IsMoving())
+        {
+            print("Moving!");
+            if (!currentMovingFX.activeSelf)
+            {
+                print("Active particles!");
+                currentMovingFX.SetActive(true);
+            }
+        }
+        else
+        {
+            print("Stopped!");
+            currentMovingFX.SetActive(false);
+        }
+    }
+
+    private bool IsMoving()
+    {
+        return (rigidbody.velocity != Vector3.zero);
+    }
+}
