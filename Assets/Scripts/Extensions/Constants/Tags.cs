db1a5e4c93e337abb671412a9e71d999926e7c76
lmatsuki <lmatsuki87@gmail.com>
2019-03-05 23:34:47 -0800

    deleting old extensions files

--- Assets/Scripts/Extensions/Constants/Tags.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Tags
-{
-    public const string Player = "Player";
-    public const string LastEnemy = "LastEnemy";
-    public const string PlayerBullet = "PlayerBullet";
-    public const string EnemyBullet = "EnemyBullet";
-    public const string Bullet = "Bullet";
-    public const string Wall = "Wall";
-}
