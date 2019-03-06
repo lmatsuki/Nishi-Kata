b5cb2d0c36ece3df1f516105e1f3581b2d535b4e
lmatsuki <lmatsuki87@gmail.com>
2019-03-05 23:33:27 -0800

    added singleton utility classes

---
+++ Assets/Scripts/Utilities/PersistentSingleton.cs
@@ -0,0 +1,11 @@
+﻿namespace NishiKata.Utilities
+{
+    public abstract class PersistentSingleton<T> : Singleton<T> where T : Singleton<T>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+namespace NishiKata.Utilities
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        //public static T instance { get; set; }
+
+        private static T s_instance;
+
+        /// <summary>
+        /// The static reference to the instance
+        /// </summary>
+        public static T instance
+        {
+            get
+            {
+                return s_instance;
+            }
+            protected set
+            {
+                s_instance = value;
+            }
+        }
+
+        public static bool InstanceExists
+        {
+            get
+            {
+                return s_instance != null;
+            }
+        }
+
+        public static event Action InstanceSet;
+
+        protected virtual void Awake()
+        {
+            if (s_instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                s_instance = (T)this;
+                if (InstanceSet != null)
+                {
+                    InstanceSet();
+                }
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (s_instance == this)
+            {
+                s_instance = null;
+            }
+        }
+    }
+}
