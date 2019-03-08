a8e2635cdf4cadbbebfe78f8a1b68baee44d9f57
lmatsuki <lmatsuki87@gmail.com>
2019-03-07 22:02:21 -0800

    minor update to Singleton class

--- Assets/Scripts/Utilities/Singleton.cs
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -1,60 +1,36 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace NishiKata.Utilities
 {
     public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
     {
-        //public static T instance { get; set; }
+        public static T instance { get; set; }
 
-        private static T s_instance;
-
-        /// <summary>
-        /// The static reference to the instance
-        /// </summary>
-        public static T instance
+        public static bool instanceExists
         {
             get
             {
-                return s_instance;
-            }
-            protected set
-            {
-                s_instance = value;
+                return instance != null;
             }
         }
-
-        public static bool InstanceExists
-        {
-            get
-            {
-                return s_instance != null;
-            }
-        }
-
-        public static event Action InstanceSet;
 
         protected virtual void Awake()
         {
-            if (s_instance != null)
+            if (instanceExists)
             {
                 Destroy(gameObject);
             }
             else
             {
-                s_instance = (T)this;
-                if (InstanceSet != null)
-                {
-                    InstanceSet();
-                }
+                instance = (T)this;
             }
         }
 
         protected virtual void OnDestroy()
         {
-            if (s_instance == this)
+            if (instance == this)
             {
-                s_instance = null;
+                instance = null;
             }
         }
     }
