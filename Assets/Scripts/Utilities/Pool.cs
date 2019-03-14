6c570fd1139fd3459ca2b0f86c9c1198e09f515f
lmatsuki <lmatsuki87@gmail.com>
2019-03-13 23:38:56 -0700

    started refactoring object poolers

---
+++ Assets/Scripts/Utilities/Pool.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NishiKata.Utilities
+{
+    public class Pool<T>
+    {
+        /// <summary>
+        /// Our factory function
+        /// </summary>
+        protected Func<T> factory;
+
+        /// <summary>
+        /// A list of all m_Available items
+        /// </summary>
+        protected readonly List<T> availableItems;
+
+        /// <summary>
+        /// A list of all items managed by the pool
+        /// </summary>
+        protected readonly List<T> allItems;
+
+        /// <summary>
+        /// Return an item to the pool
+        /// </summary>
+        public virtual void Return(T pooledItem)
+        {
+            if (allItems.Contains(pooledItem) &&
+                !availableItems.Contains(pooledItem))
+            {
+                ReturnToPoolInternal(pooledItem);
+            }
+            else
+            {
+                throw new InvalidOperationException("Trying to return an item to a pool that does not contain it: " + pooledItem +
+                                                    ", " + this);
+            }
+        }
+
+        /// <summary>
+        /// Returns an object to the m_Available list. Does not check for consistency
+        /// </summary>
+        protected virtual void ReturnToPoolInternal(T element)
+        {
+            availableItems.Add(element);
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/PoolManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace NishiKata.Utilities
+{
+    public class PoolManager : Singleton<PoolManager>
+    {
+        /// <summary>
+        /// List of poolables that will be used to initialize corresponding pools
+        /// </summary>
+        public List<Poolable> poolables;
+
+        /// <summary>
+        /// Dictionary of pools, key is the prefab
+        /// </summary>
+        //protected Dictionary<Poolable, AutoComponentPrefabPool<Poolable>> m_Pools;
+
+        ///// <summary>
+        ///// Gets a poolable component from the corresponding pool
+        ///// </summary>
+        ///// <param name="poolablePrefab"></param>
+        ///// <returns></returns>
+        //public Poolable GetPoolable(Poolable poolablePrefab)
+        //{
+        //    if (!m_Pools.ContainsKey(poolablePrefab))
+        //    {
+        //        m_Pools.Add(poolablePrefab, new AutoComponentPrefabPool<Poolable>(poolablePrefab, Initialize, null,
+        //                                                                          poolablePrefab.initialPoolCapacity));
+        //    }
+
+        //    AutoComponentPrefabPool<Poolable> pool = m_Pools[poolablePrefab];
+        //    Poolable spawnedInstance = pool.Get();
+
+        //    spawnedInstance.pool = pool;
+        //    return spawnedInstance;
+        //}
+    }
+}
