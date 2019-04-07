54dc20f4be0ea7f091e3ab0a662eb291084ffd67
lmatsuki <lmatsuki87@gmail.com>
2019-04-06 23:30:40 -0700

    added pausing/resuming of song when pausing

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using NishiKata.Audio;
+using UnityEngine;
+
+namespace NishiKata.UI
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        public GameObject pauseMenu;
+
+        private string songName;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            if (PressedPause())
+            {
+                pauseMenu.SetActive(!pauseMenu.activeSelf);
+
+                if (pauseMenu.activeSelf)
+                {
+                    songName = AudioManager.instance.GetCurrentlyPlayingSongName();
+                    AudioManager.instance.PauseSong(songName);
+                }
+                else
+                {
+                    AudioManager.instance.PlaySong(songName);
+                }
+            }
+        }
+
+        private bool PressedPause()
+        {
+            return Input.GetKeyUp(KeyCode.Escape);
+        }
+    }
+
+}
